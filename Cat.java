22807ee349de3701447f336758582deb783eaf2a
chienka <chienkahaui@gmail.com>
2017-11-20 15:55:53 +0900

    First Commit

---
+++ Cat.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Cat {
+    public static void main(String args[]){
+        while(true) {
+            String s;
+
+            Scanner scn = new Scanner(System.in);
+            System.out.print("");
+            s = scn.nextLine();
+            System.out.printf("%s\n", s);
+            if(s.equals("exit()")){
+                break;
+            }
+
+        }
+    }
+}
