94bb5a4e6ca2032353568db4534c1374736fe0b6
chienka <chienkahaui@gmail.com>
2017-11-21 16:12:26 +0900

    commit

--- Cat.java
+++ Cat.java
@@ -1,15 +1,27 @@
+import java.io.*;
 import java.util.Scanner;
 
 public class Cat {
-    public static void main(String args[]){
-        while(true) {
+
+
+    public static void main(String args[])
+
+    {
+        File file = new File("Text.txt"); // create file textfile.txt
+        String absolutePath = file.getAbsolutePath();
+
+        System.out.println(file.getAbsolutePath());
+        System.out.println(file.getPath());
+
+//        String filePath = absolutePath.substring(0, absolutePath.lastIndexOf(File.separator));
+//        System.out.println("File Path : " + filePath);
+        while (true) {
             String s;
 
             Scanner scn = new Scanner(System.in);
-//            System.out.print("");
             s = scn.nextLine();
             System.out.printf("%s\n", s);
-            if(s.equals("exit()")){
+            if (s.equals("exit()")) {
                 break;
             }
 
