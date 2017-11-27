22be2b1d746fe9f6725fc6771bcae56e34c8efd7
chienka <chienkahaui@gmail.com>
2017-11-27 12:20:52 +0900

    update Cat.java
    add -n option

--- Cat.java
+++ Cat.java
@@ -1,36 +1,68 @@
+
+
+
 import java.io.*;
 
 public class Cat {
 
     static void printFile(BufferedReader src) {
         try {
-            String  currentline;
+            String currentline;
 //read next character
             while ((currentline = src.readLine()) != null)
-//                write character
+//write character
                 System.out.println(currentline);
             src.close();
+        } catch (IOException e) {
         }
-        catch (IOException e) { }
+    }
+
+    static void printFileOpt(File fileName) throws FileNotFoundException {
+
+
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
+        LineNumberReader lineNumberReader = new LineNumberReader(new FileReader(fileName));
+        try {
+//read next character
+            while (lineNumberReader.readLine() != null)
+                System.out.println(lineNumberReader.getLineNumber() + "\t" + bufferedReader.readLine());
+//write character
+//            System.out.println(currentline);
+            bufferedReader.close();
+            lineNumberReader.close();
+        } catch (IOException e) {
+        }
     }
 
     public static void main(String args[])
 
     {
+        boolean opt = false;
         if (args.length < 1)
             // fileName is null, read data from input
             printFile(new BufferedReader(new InputStreamReader(System.in)));
         else {
             for (int i = 0; i < args.length; i++) {
-                if (args[i].equals("cat")){
+
+                if (args[i].equals("cat")) {
+                    continue;
+                }
+                if (args[i].equals("-n")) {
+                    opt = true;
                     continue;
                 }
                 try {
-                    printFile(new BufferedReader(new FileReader(args[i])));
+                    File file = new File(args[i]);
+                    System.out.println(file.getAbsolutePath());
+                    if (opt) {
+                        printFileOpt(file);
+                    } else {
+                        printFile(new BufferedReader(new FileReader(args[i])));
+                    }
+                } catch (FileNotFoundException e) {
+                    System.err.println("File " + args[i] + "can`t open!");
                 }
-                catch (FileNotFoundException e) {
-                    System.err.println("File" + args[i] + "can`t open!");
-                }
+
             }
         }
     }
